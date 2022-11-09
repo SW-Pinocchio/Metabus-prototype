f6fc98b765e3cd93eb14efdf0cc73f00cba301d5
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2022-11-09 23:13:58 +0900

    Feat/#001 character (#2)
    * chore: update .gitignore
    
    * feat: add player interacting with objects
    
    * chore: change Start functions into Awake functions
    Awake functions are used to initialize variables of each components.
    Start functions will be used later to pass information between components.
    
    * chore: change the name RaycastScript into RaycastController
    The name 'RaycastController' is used for consistency in names.
    
    * feat: add jumping

---
+++ testmetaverse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        Cursor.visible = true;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- testmetaverse/Assets/Scripts/TPSCharacterController.cs
+++ testmetaverse/Assets/Scripts/TPSCharacterController.cs
@@ -10,22 +10,41 @@ public class TPSCharacterController : MonoBehaviour
     private Transform cameraArm;
 
     Animator animator;
+    Rigidbody rigid;
 
-    void Start()
+    bool isJumpingUp; // 위로 점프 중
+    //bool isJumpingDown; // 점프하고 아래로 떨어지는 중
+    bool isOnGround; // 플레이어가 땅 위에 서있는지 체크
+
+    void Awake()
     {
         animator = characterBody.GetComponent<Animator>();
+        rigid = characterBody.GetComponent<Rigidbody>();
+        isOnGround = true;
     }
+
     void Update()
     {
-        LookAround();
         Move();
+        InteractObj();
     }
+
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+
+    private void LateUpdate()
+    {
+        LookAround();
+    }
+
     private void LookAround()
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         Vector3 camAngle = cameraArm.rotation.eulerAngles;
 
-        //추가
+        // 카메라 각도 제한 (마우스를 움직이다보면 화면이 뒤집어지는 문제 발생)
         float x = camAngle.x - mouseDelta.y;
         if (x < 180f)
         {
@@ -42,9 +61,9 @@ public class TPSCharacterController : MonoBehaviour
     private void Move()
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        bool isMove = moveInput.magnitude != 0;
-        animator.SetBool("isMove", isMove);
-        if (isMove)
+        bool isMoving = moveInput.magnitude != 0;
+        animator.SetBool("isMoving", isMoving);
+        if (isMoving)
         {
             Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
             Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
@@ -52,6 +71,34 @@ public class TPSCharacterController : MonoBehaviour
 
             characterBody.forward = moveDir;
             transform.position += moveDir * Time.deltaTime * 5f;
+        }
+    }
+
+    private void InteractObj()
+    {
+        if (Input.GetMouseButton(0))
+            animator.SetBool("isInteracting", true);
+        else
+            animator.SetBool("isInteracting", false);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rigid.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isOnGround = false;
+            Debug.Log("Jumping");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            Debug.Log("On the Ground");
+            isOnGround = true;
         }
     }
 }
