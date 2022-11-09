6f54ce923d3c66e2d7c90e2c7b65b163086b686b
sarah2234 <2234sarah@gmail.com>
2022-11-09 15:13:09 +0900

    chore: change the name RaycastScript into RaycastController The name 'RaycastController' is used for consistency in names.

--- testmetaverse/Assets/Scripts/RaycastController.cs
+++ testmetaverse/Assets/Scripts/RaycastController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RaycastScript : MonoBehaviour
+public class RaycastController : MonoBehaviour
 {
     Ray ray;
     RaycastHit hit;
